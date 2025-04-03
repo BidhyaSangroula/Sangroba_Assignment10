55c81f113336f6a255e46121a192fb00f6574ce0
Bidhya Sangroula <sangroba@mail.uc.edu>
2025-04-03 12:05:38 -0400

    Add project files.

---
+++ Sangroba_Assignment10/My Interest.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sangroba_Assignment10
+{
+    public partial class My_Interest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
